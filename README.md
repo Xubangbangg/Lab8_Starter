74677b4889fb7fa25c29f2a53f45391652d3c615
xubangbang <mix006@ucsd.edu>
2021-11-12 00:30:18 -0800

    finish lab

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # Lab 8 - Starter
+
+Minjun Xu
+
+1) In your own words: Where would you fit your automated tests in your Recipe project development pipeline?
+
+Within a Github action that runs whenever code is pushed 
+
+2) Would you use an end to end test to check if a function is returning the correct output? 
+   No
+
+3) Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user.
+
+No,I believe it's hard to check between two users since they need intenert connections and interactions of them. 
+
+4) Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not? For this question, assume the “max message length” feature prevents the user from typing more than 80 characters.
+
+Yes, since that's a small scale and doesn't interact with other. 
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,109 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+
+//isPhoneNumber 4 tests
+test('isPhoneNumber True1', () => {
+    res = functions.isPhoneNumber('858-349-3948');
+    expect(res).toBe(true);
+});
+
+test('isPhoneNumber True2', () => {
+    res = functions.isPhoneNumber('(858)349-3948');
+    expect(res).toBe(true);
+});
+
+test('isPhoneNumber False1', () => {
+    res = functions.isPhoneNumber('858-349-394');
+    expect(res).toBe(false);
+});
+
+test('isPhoneNumber False2', () => {
+    res = functions.isPhoneNumber('8583493948');
+    expect(res).toBe(false);
+});
+
+//isemail 4 tests
+test('isEmail True1', () => {
+    res = functions.isEmail('mix006@ucsd.edu');
+    expect(res).toBe(true);
+});
+
+test('isEmail True2', () => {
+    res = functions.isEmail('mix006__@ucsd.edu');
+    expect(res).toBe(true);
+});
+
+test('isEmail False1', () => {
+    res = functions.isEmail('mix006@ucsd');
+    expect(res).toBe(false);
+});
+
+test('isEmail False2', () => {
+    res = functions.isEmail('mix006djaihfoa');
+    expect(res).toBe(false);
+});
+
+//isStrongPassword 4 tests
+test('isStrongPassword True1', () => {
+    res = functions.isStrongPassword('lolol');
+    expect(res).toBe(true);
+});
+
+test('isStrongPassword True2', () => {
+    res = functions.isStrongPassword('lolol123');
+    expect(res).toBe(true);
+});
+
+test('isStrongPassword False1', () => {
+    res = functions.isStrongPassword('hh');
+    expect(res).toBe(false);
+});
+
+test('isStrongPassword False2', () => {
+    res = functions.isStrongPassword('1234');
+    expect(res).toBe(false);
+});
+
+//isDate 4 tests
+test('isDate True1', () => {
+    res = functions.isDate('1/1/2000');
+    expect(res).toBe(true);
+});
+
+test('isDate True2', () => {
+    res = functions.isDate('11/11/2021');
+    expect(res).toBe(true);
+});
+
+test('isDate False1', () => {
+    res = functions.isDate('11/11/21');
+    expect(res).toBe(false);
+});
+
+
+test('isDate False2', () => {
+    res = functions.isDate('11/111/21');
+    expect(res).toBe(false);
+});
+
+//isHexColor 4 tests
+test('isHexColor True1', () => {
+    res = functions.isHexColor('#AA5533');
+    expect(res).toBe(true);
+});
+
+test('isHexColor True2', () => {
+    res = functions.isHexColor('#AF1233');
+    expect(res).toBe(true);
+});
+
+test('isHexColor False1', () => {
+    res = functions.isHexColor('#');
+    expect(res).toBe(false);
+});
+
+test('isHexColor False2', () => {
+    res = functions.isHexColor('#1');
+    expect(res).toBe(false);
+});
